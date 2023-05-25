bd3bf8af34f85430f24a68ff381b0464824eb32a
unknown <jahanzaib1999@gmail.com>
2023-05-25 19:10:18 +1000

    dont env added

--- src/hooks/useTmdb.js
+++ src/hooks/useTmdb.js
@@ -4,13 +4,12 @@ const useTmdb = (endpoint, params = "") => {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const apiKey = "ed036b8de8d4dd1405534d162870b1fd";
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          `https://api.themoviedb.org/3${endpoint}?api_key=${apiKey}&${params}`
+          `https://api.themoviedb.org/3${endpoint}?api_key=${process.env.REACT_APP_API_KEY}&${params}`
         );
         if (!response.ok) {
           throw new Error(response.statusText);
